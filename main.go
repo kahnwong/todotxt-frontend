89441a1a398ad741f8424d80412c28cc1473c2b2
Karn Wong <karn@karnwong.me>
2024-12-27 01:57:15 +0700

    template: add static from web inspector

--- main.go
+++ main.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/gofiber/fiber/v2"
 )
 
+//func getTodos() []string {
+//	return []string{"a", "b", "c", "d"}
+//}
+
 func main() {
+	// init
 	app := fiber.New()
 
-	app.Static("/", "./spa")
+	// render site
+	//app.Static("/", "./spa")
 
+	//---- Render index
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("./static/index.html", fiber.Map{
+			"Todo": "foo"})
+	})
+
+	// start server
 	err := app.Listen(":3000")
 	if err != nil {
 		fmt.Println("Error starting server", err)
