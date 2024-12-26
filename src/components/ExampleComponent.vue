9c4881d0f88e0360b9423df90613051e3f90eb1c
Karn Wong <karn@karnwong.me>
2024-12-26 22:58:47 +0700

    quasar: add list ui component

--- src/components/ExampleComponent.vue
+++ src/components/ExampleComponent.vue
@@ -1,37 +1,20 @@
 <template>
-  <div>
-    <p>{{ title }}</p>
-    <ul>
-      <li v-for="todo in todos" :key="todo.id" @click="increment">
-        {{ todo.id }} - {{ todo.content }}
-      </li>
-    </ul>
-    <p>Count: {{ todoCount }} / {{ meta.totalCount }}</p>
-    <p>Active: {{ active ? 'yes' : 'no' }}</p>
-    <p>Clicks on todos: {{ clickCount }}</p>
+  <div class="q-pa-md" style="max-width: 350px">
+    <q-list bordered separator>
+      <q-item clickable v-ripple>
+        <q-item-section>Single line item</q-item-section>
+      </q-item>
+
+      <q-item clickable v-ripple>
+        <q-item-section>Single line item</q-item-section>
+      </q-item>
+
+      <q-item clickable v-ripple>
+        <q-item-section>Single line item</q-item-section>
+      </q-item>
+    </q-list>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue'
-import type { Todo, Meta } from './models'
-
-interface Props {
-  title: string
-  todos?: Todo[]
-  meta: Meta
-  active: boolean
-}
-
-const props = withDefaults(defineProps<Props>(), {
-  todos: () => [],
-})
-
-const clickCount = ref(0)
-function increment() {
-  clickCount.value += 1
-  return clickCount.value
-}
-
-const todoCount = computed(() => props.todos.length)
 </script>
