802a639751378cc3825b736377f7301eeb36da39
Karn Wong <karn@karnwong.me>
2024-12-27 12:52:14 +0700

    integrate dummy values with template

--- main.go
+++ main.go
@@ -2,24 +2,22 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 )
 
-//func getTodos() []string {
-//	return []string{"a", "b", "c", "d"}
-//}
+func getTodos() []string {
+	return []string{"a", "b", "c", "d"}
+}
 
 func main() {
 	// init
 	app := fiber.New()
 
 	// render site
-	//app.Static("/", "./spa")
-
-	//---- Render index
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Render("./static/index.html", fiber.Map{
-			"Todo": "foo"})
+			"Items": getTodos()})
 	})
 
 	// start server
